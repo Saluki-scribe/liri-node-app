58e89ae95de6816bf40527632335e0c5beec199b
saluki-scribe <tara@nelsonwriting.com>
2017-09-25 20:29:25 -0400

    Split twitter and spotify key object into separate files and adjusted code in liri.js. Deleted test.js.

---
+++ twitter-keys.js
@@ -0,0 +1,12 @@
+console.log('this is loaded');
+
+//Twitter Key Object
+
+var twitterKeys = {
+  consumer_key: 'cKR9j3ppBKuhovDl4ozV6Bn1P',
+  consumer_secret: 'CK0MJ1fLz1hlM4VCjRcGwG0rYIfhRdGnRNPGdmwJHQNbtq4eqx',
+  access_token_key: '911729161017970694-jIHrAMmTFulMUg27beyuLlY1Ao7YDVL',
+  access_token_secret: 'FgVPQsAhHDWMYB5mnaWXFgh8aOmxLursbRpfkxqgnOZrg'
+}
+
+module.exports = twitterKeys;
